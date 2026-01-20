92cd06a3654359d1f024802cff0a84416d26688a
Zhao Yinpeng <76549739+zyp9611@users.noreply.github.com>
2026-01-20 09:15:46 +0800

    Update check-in token and improve error handling

--- checkin.py
+++ checkin.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         origin = "https://glados.space"
         useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
         payload = {
-            'token': 'glados.one'
+            'token': 'glados.cloud'
         }
         
         for cookie in cookies:
@@ -95,3 +95,4 @@ if __name__ == '__main__':
     else:
         pushdeer = PushDeer(pushkey=sckey) 
         pushdeer.send_text(title, desp=context)
+
